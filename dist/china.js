f17fcf0145d4ed7bdae02bf80ec4cf87d7a7a3e2
jeffreyshen19 <jshen@andover.edu>
2018-09-30 18:30:27 -0400

    improve aesthetic slightly

--- dist/china.js
+++ dist/china.js
@@ -4,7 +4,7 @@ function displayPosts(posts, lastSeen){
     .enter()
     .append("li")
     .html(function(d){
-      return "<img class = 'profile-photo' src = '../assets/graphics/profile-photo.png'><h2>" + ("" + d.uid).substring(0, 5) + "*****</h2><br><p class = 'text'>" + d.text + "</p><br><span>RECEIVED:</span> " + d.retrieved + "<br><span>LAST SEEN:</span> " + lastSeen + "<br>";
+      return "<img class = 'profile-photo' src = '../assets/graphics/profile-photo.png'><h2>" + ("" + d.uid).substring(0, 5) + "*****</h2><br><p class = 'text'>" + d.text + "</p><br><span>LAST SEEN:</span> " + d.retrieved + "<br>";
     });
 }
 
@@ -13,7 +13,6 @@ function updatePosts(){
   $("#loading").show();
 
   $.get("https://cs.dpccdn.net/v1/censored_posts", function(data){
-    console.log(data);
     //Display Posts
     d3.select("#posts").selectAll("*").remove(); //Get rid of existing children
     displayPosts(data.posts, data.lastUpdated);
