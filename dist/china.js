6a3484a330cb2a7a567a733215c724c0d319ce3f
jeffreyshen19 <jshen@andover.edu>
2018-09-30 18:23:03 -0400

    updated

--- dist/china.js
+++ dist/china.js
@@ -1,19 +1,28 @@
-var posts = [{
-  "uid": "2571906604",
-  "text": "你好，你怎么样?",
-  "retrieved": "2018-09-29 (11:06:01.560) EDT",
-  "last_seen": "2018-09-29 (11:06:01.560) EDT"
-}, {
-  "uid": "2571906604",
-  "text": "你好，你怎么样?",
-  "retrieved": "2018-09-29 (11:06:01.560) EDT",
-  "last_seen": "2018-09-29 (11:06:01.560) EDT"
-}];
+function displayPosts(posts, lastSeen){
+  d3.select("#posts").selectAll("li")
+    .data(posts)
+    .enter()
+    .append("li")
+    .html(function(d){
+      return "<img class = 'profile-photo' src = '../assets/graphics/profile-photo.png'><h2>" + ("" + d.uid).substring(0, 5) + "*****</h2><br><p class = 'text'>" + d.text + "</p><br><span>RECEIVED:</span> " + d.retrieved + "<br><span>LAST SEEN:</span> " + lastSeen + "<br>";
+    });
+}
 
-d3.select("#posts").selectAll("li")
-  .data(posts)
-  .enter()
-  .append("li")
-  .html(function(d){
-    return "<img class = 'profile-photo' src = '../assets/graphics/profile-photo.png'><h2>" + d.uid.substring(0, 5) + "*****</h2><br><p class = 'text'>" + d.text + "</p><br><span>RECEIVED:</span> " + d.retrieved + "<br><span>LAST SEEN:</span> " + d.last_seen + "<br>";
+function updatePosts(){
+  //Add the loading icon
+  $("#loading").show();
+
+  $.get("https://cs.dpccdn.net/v1/censored_posts", function(data){
+    console.log(data);
+    //Display Posts
+    d3.select("#posts").selectAll("*").remove(); //Get rid of existing children
+    displayPosts(data.posts, data.lastUpdated);
+
+    //Remove the loading icon
+    $("#loading").hide();
   });
+}
+
+$(document).ready(function(){
+  updatePosts();
+});
